bd08e892d0cfa34463a738ea3b687e537901be02
Thiago Campanilli <thiago.campanilli@claro.com.br>
2025-12-29 16:40:18 -0300

    ajustes

--- api/auth-login.js
+++ api/auth-login.js
@@ -1,5 +1,11 @@
 import jwt from 'jsonwebtoken';
 import cookie from 'cookie';
+import bcrypt from 'bcryptjs';
+
+const USER = 'admin';
+
+// HASH gerado previamente
+const PASSWORD_HASH = '$2a$12$K6z1s9Y5ZpZ4e5X7xqz3XuGZk0n1xP0z3E0Zz4pL8pV1yN1R7S9tO';
 
 export default function handler(req, res) {
   if (req.method !== 'POST') {
@@ -8,8 +14,11 @@ export default function handler(req, res) {
 
   const { user, pass } = req.body;
 
-  if (user !== 'admin' || pass !== '123') {
-    return res.status(401).json({ success: false });
+  if (user !== USER || !bcrypt.compareSync(pass, PASSWORD_HASH)) {
+    return res.status(401).json({
+      success: false,
+      message: 'Usuário ou senha inválidos'
+    });
   }
 
   const token = jwt.sign(
